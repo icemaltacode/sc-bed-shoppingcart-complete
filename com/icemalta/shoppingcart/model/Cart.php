030c76bcc25b989126cea3fcbc974b361af2967e
Keith Vassallo <keith@icemalta.com>
2023-11-17 11:16:48 +0000

    Fixed some formatting.

--- com/icemalta/shoppingcart/model/Cart.php
+++ com/icemalta/shoppingcart/model/Cart.php
@@ -1,30 +1,35 @@
 <?php
 namespace com\icemalta\shoppingcart\model;
 
-class CartItem {
+class CartItem
+{
     public Product $product;
     public int $qty;
 
-    public function __construct(Product $product, int $qty) {
+    public function __construct(Product $product, int $qty)
+    {
         $this->product = $product;
         $this->qty = $qty;
     }
 
-    public function getSubtotal(): float {
+    public function getSubtotal(): float
+    {
         return $this->qty * $this->product->getPrice();
     }
 }
 
-class Cart {
+class Cart
+{
     public array $cartItems = [];
 
-    public function upsertProduct(Product $product, int $qty): array {
+    public function upsertProduct(Product $product, int $qty): array
+    {
         if ($qty < 1) {
             return $this->removeProduct($product->getId());
         }
-        
+
         $item = null;
-        foreach( $this->cartItems as &$cartItem ) {
+        foreach ($this->cartItems as &$cartItem) {
             if ($cartItem->product->getId() === $product->getId()) {
                 $item = &$cartItem;
             }
@@ -40,27 +45,30 @@ class Cart {
         return $this->cartItems;
     }
 
-    public function removeProduct(int $productId): array {
-        foreach($this->cartItems as $key=>$cartItem) {
+    public function removeProduct(int $productId): array
+    {
+        foreach ($this->cartItems as $key => $cartItem) {
             if ($cartItem->product->getId() === $productId) {
-                unset( $this->cartItems[$key] );
+                unset($this->cartItems[$key]);
             }
         }
-        
+
         return $this->cartItems;
     }
 
-    public function getTotal() : float {
+    public function getTotal(): float
+    {
         $total = 0;
-        foreach($this->cartItems as $cartItem) {
+        foreach ($this->cartItems as $cartItem) {
             $total += $cartItem->product->getPrice() * $cartItem->qty;
         }
         return $total;
     }
 
-    public function getItemCount() : int {
+    public function getItemCount(): int
+    {
         $total = 0;
-        foreach($this->cartItems as $cartItem) {
+        foreach ($this->cartItems as $cartItem) {
             $total += $cartItem->qty;
         }
         return $total;
--- com/icemalta/shoppingcart/model/Product.php
+++ com/icemalta/shoppingcart/model/Product.php
@@ -1,7 +1,8 @@
 <?php
 namespace com\icemalta\shoppingcart\model;
 
-class Product {
+class Product
+{
     private int $id;
     private string $name;
     private float $price;
@@ -9,7 +10,8 @@ class Product {
     private ?string $featuredImage = null;
     private bool $requiresDeposit = false;
 
-    public function __construct(int $id, string $name, float $price, string $description, string $featuredImage = null, bool $requiresDeposit = false) {
+    public function __construct(int $id, string $name, float $price, string $description, string $featuredImage = null, bool $requiresDeposit = false)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
@@ -18,56 +20,68 @@ class Product {
         $this->requiresDeposit = $requiresDeposit;
     }
 
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function setId(int $id): self {
+    public function setId(int $id): self
+    {
         $this->id = $id;
         return $this;
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function setName(string $name): self {
+    public function setName(string $name): self
+    {
         $this->name = $name;
         return $this;
     }
 
-    public function getPrice(): float{
+    public function getPrice(): float
+    {
         return $this->price;
     }
 
-    public function setPrice(float $price): self {
+    public function setPrice(float $price): self
+    {
         $this->price = $price;
         return $this;
     }
 
-    public function getDescription(): string {
+    public function getDescription(): string
+    {
         return $this->description;
     }
 
-    public function setDescription(string $description): self {
+    public function setDescription(string $description): self
+    {
         $this->description = $description;
         return $this;
     }
 
-    public function getFeaturedImage(): ?string {
+    public function getFeaturedImage(): ?string
+    {
         return $this->featuredImage;
     }
 
-    public function setFeaturedImage(?string $featuredImage): self {
+    public function setFeaturedImage(?string $featuredImage): self
+    {
         $this->featuredImage = $featuredImage;
         return $this;
     }
 
-    public function requiresDeposit(): bool {
+    public function requiresDeposit(): bool
+    {
         return $this->requiresDeposit;
     }
 
-    public function setRequiresDeposit(bool $requiresDeposit): self {
+    public function setRequiresDeposit(bool $requiresDeposit): self
+    {
         $this->requiresDeposit = $requiresDeposit;
         return $this;
     }
